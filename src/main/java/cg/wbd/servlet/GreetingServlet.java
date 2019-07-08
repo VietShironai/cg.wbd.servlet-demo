db0b0816eef6288e3b2ab169efa02a34694716a5
edward <son@binhsonnguyen.com>
2019-07-09 04:37:45 +0700

    change form's submit method to POST

--- src/main/java/cg/wbd/servlet/GreetingServlet.java
+++ src/main/java/cg/wbd/servlet/GreetingServlet.java
@@ -10,18 +10,12 @@ public class GreetingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         StringBuilder stringBuilder = new StringBuilder()
-                .append("<form>")
+                .append("<form method=\"post\">")
                 .append("    <label>")
                 .append("        <input type=\"text\" name=\"name\">")
                 .append("    </label>")
                 .append("    <input type=\"submit\" value=\"Submit\">")
                 .append("</form>");
-        String nameParam = req.getParameter("name");
-        if (nameParam == null || nameParam.isEmpty()) {
-            stringBuilder.append("Hello world!");
-        } else {
-            stringBuilder.append("Hello ").append(nameParam).append("!");
-        }
         resp.setContentType("text/html");
         resp.getOutputStream().println(stringBuilder.toString());
     }
