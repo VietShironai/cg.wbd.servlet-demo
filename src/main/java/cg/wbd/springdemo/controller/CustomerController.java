13326e448fd16e35a2f2c604b67300f8c138e156
edward <son@binhsonnguyen.com>
2019-07-10 15:07:01 +0700

    mark CustomerController class as a spring controller

--- src/main/java/cg/wbd/springdemo/controller/CustomerController.java
+++ src/main/java/cg/wbd/springdemo/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package cg.wbd.springdemo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class CustomerController {
     @GetMapping("customers")
     public String list() {
