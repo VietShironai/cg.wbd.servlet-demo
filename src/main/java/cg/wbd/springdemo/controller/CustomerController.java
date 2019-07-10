763b852fdf391971195af4b8a44f2ce12ade24db
edward <son@binhsonnguyen.com>
2019-07-10 15:28:28 +0700

    add a dummy customers list for list view

--- src/main/java/cg/wbd/springdemo/controller/CustomerController.java
+++ src/main/java/cg/wbd/springdemo/controller/CustomerController.java
@@ -1,12 +1,17 @@
 package cg.wbd.springdemo.controller;
 
+import cg.wbd.springdemo.model.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class CustomerController {
     @GetMapping("customers")
-    public String list() {
+    public String list(Model model) {
+        model.addAttribute("customers", new ArrayList<Customer>());
         return "customers/list.jsp";
     }
 }
