b953c98ec924ce86e0259316cdc9a0fd09fc6b2e
edward <son@binhsonnguyen.com>
2019-07-10 15:55:33 +0700

    use ModelAndView instead of Model reference

--- src/main/java/cg/wbd/springdemo/controller/CustomerController.java
+++ src/main/java/cg/wbd/springdemo/controller/CustomerController.java
@@ -2,16 +2,17 @@ package cg.wbd.springdemo.controller;
 
 import cg.wbd.springdemo.model.Customer;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
 
 @Controller
 public class CustomerController {
     @GetMapping("customers")
-    public String list(Model model) {
-        model.addAttribute("customers", new ArrayList<Customer>());
-        return "customers/list.jsp";
+    public ModelAndView list() {
+        ModelAndView modelAndView = new ModelAndView("customers/list.jsp");
+        modelAndView.addObject("customers", new ArrayList<Customer>());
+        return modelAndView;
     }
 }
